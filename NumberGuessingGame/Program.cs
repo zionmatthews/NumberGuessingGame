e7b2f486e6d2fa2f75b39cde77b51ba4e426c6d5
zion matthews <zman5682@gmail.com>
2019-09-11 17:29:35 -0500

    Add project files.

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+         
+        
+        static void Main(string[] args)
+        {
+            
+
+            string choice = "";
+
+            //Asking the player what to do.
+            Console.WriteLine("Please think of a whole number between 1-100");
+            Console.WriteLine("");
+            Console.WriteLine("When you're done. Please press enter");
+            Console.ReadLine();
+
+            if(choice == "enter")
+            {
+                NumberGuess();
+            }
+
+            //Generating the random number
+            Random rand = new Random();
+            rand.Next(1, 100);
+
+
+           
+
+        }
+        public static void NumberGuess(Random rand = new Random())
+        {
+            bool correct = true;           
+
+            while (rand != correct)
+            {
+
+
+                foreach(int rand   ) 
+
+
+
+
+
+
+            }
+        }
+
+    }
+}
